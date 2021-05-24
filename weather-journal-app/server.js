6fb0a1dfeedd293551320a21f40cf9a3634b5e99
Max Girteit <icufishmg@gmail.com>
2021-05-24 13:41:45 -0600

    switched from body parser middleware to express included json parser

--- weather-journal-app/server.js
+++ weather-journal-app/server.js
@@ -2,18 +2,33 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require("express");
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
+// const bodyParser = require('body-parser'); deprecated
+
 //Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 // Cors for cross origin allowance
+const cors = require("cors");
+app.use(cors());
 
 // Initialize the main project folder
-app.use(express.static('website'));
-
+app.use(express.static("website"));
 
 // Setup Server
+const port = 3180;
+
+const server = app.listen(port, () => {
+  console.log(`server is listening on port ${port}`);
+});
+
+// const listening = () => {
+//     console.log('server running');
+//     console.log(`server is listening on port ${port}`);
+// }
