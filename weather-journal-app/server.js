790873368a45dc42da1f669eeb435ec0fd2fed51
Max Girteit <icufishmg@gmail.com>
2021-06-10 07:00:41 -0600

    finished writing commments, app working

--- weather-journal-app/server.js
+++ weather-journal-app/server.js
@@ -1,18 +1,16 @@
-
-
-
 // Setup empty JS object to act as endpoint for all routes
 projectData = [];
 
 // Require Express to run server and routes
-const express = require('express');
+const express = require("express");
 
 // Start up an instance of app
 const app = express();
 
 /* Middleware*/
 // const bodyParser = require('body-parser'); deprecated
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
+
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -26,29 +24,21 @@ app.use(express.static("website"));
 
 // Setup Server
 const port = 8000;
+const server = app.listen(port, () =>
+  console.log(`server is listening on port ${port}`)
+);
 
-const server = app.listen(port, () => console.log(`server is listening on port ${port}`));
+//get and post route for receiving and sending data from app.js
+app.get("/projectdata", (req, res) => res.send(projectData));
+app.post("/weatherdata", saveWeather);
 
-app.get('/projectdata', (req, res) => res.send(projectData));
-app.post('/weatherdata', saveWeather)
-
+//callback function to save data received from app.js to projectData array
 function saveWeather(req, res) {
   let data = req.body;
-  // console.log(data)
   let newEntry = {
     temperature: data.temperature,
     date: data.date,
-    feeling: data.feeling
+    feeling: data.feeling,
   };
   projectData.push(newEntry);
-  console.log(projectData);
-};
-
-
-
-
-
-
-
-
-
+}
--- weather-journal-app/website/app.js
+++ weather-journal-app/website/app.js
@@ -2,20 +2,26 @@
 const apiKey = "&appid=0bee29c772704d38dee97aa6f80bcb11";
 const baseURL = "http://api.openweathermap.org/data/2.5/weather?zip=";
 
-// const url = baseURL + zipCode + apiKey;
+// event listener that initiates api call;
 
 document.getElementById("generate").addEventListener("click", performAction);
 
+// callback function called by event listener
 function performAction(e) {
   const userResponse = document.getElementById("feelings").value;
   const zipCode = document.getElementById("zip").value;
+
+  //api call chained with server.js call and UI update of data from api and user
   getWeather(baseURL, zipCode, apiKey).then(function (data) {
-    console.log(data);
+    //convert kelvin to farenheit and round up number
+    const tempRound = `${
+      Math.round((((data.main.temp - 273.15) * 9) / 5 + 32) * 10) / 10
+    }°F`;
     postData("/weatherdata", {
-      temperature: data.main.temp,
-      date: d,
+      temperature: tempRound,
+      date: newDate,
       feeling: userResponse,
-    });
+    }).then(updateUI());
   });
 }
 
@@ -23,19 +29,19 @@ function performAction(e) {
 let d = new Date();
 let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
 
+//api call function
 const getWeather = async (baseURL, zipCode, apiKey) => {
   const res = await fetch(baseURL + zipCode + apiKey);
   try {
     const data = await res.json();
-    console.log(data);
+
     return data;
   } catch (error) {
     console.log("error", error);
   }
 };
-
+// save data to server with POST
 const postData = async (url = "", data = {}) => {
-  // console.log(data);
   const response = await fetch(url, {
     method: "POST",
     credentials: "same-origin",
@@ -44,4 +50,23 @@ const postData = async (url = "", data = {}) => {
     },
     body: JSON.stringify(data),
   });
+};
+//update UI with data from server.js and user input feelings
+const updateUI = async () => {
+  const request = await fetch("/projectdata");
+  try {
+    const allData = await request.json();
+    document.getElementById("entry").className = "holderResponse";
+    document.getElementById("content").innerHTML = `Feeling: ${
+      allData.slice(-1)[0].feeling
+    }`;
+    document.getElementById("date").innerHTML = `Date: ${
+      allData.slice(-1)[0].date
+    }`;
+    document.getElementById("temp").innerHTML = `Temperature: ${
+      allData.slice(-1)[0].temperature
+    }`;
+  } catch (error) {
+    console.log("error", error);
+  }
 };
